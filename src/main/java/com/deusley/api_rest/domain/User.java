d6ee30dcbb2b2c5977dbf0dd3aebe6a314243c56
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2023-03-07 15:51:36 -0300

    implDomainClasss

---
+++ src/main/java/com/deusley/api_rest/domain/User.java
@@ -0,0 +1,39 @@
+package com.deusley.api_rest.domain;
+
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@ToString
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
