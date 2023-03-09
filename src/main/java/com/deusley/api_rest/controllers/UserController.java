e6f4572762171e164f31dcf5d53e7aa0404e3392
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2023-03-09 03:10:15 -0300

    Mapper/DTO

--- src/main/java/com/deusley/api_rest/controllers/UserController.java
+++ src/main/java/com/deusley/api_rest/controllers/UserController.java
@@ -1,7 +1,8 @@
 package com.deusley.api_rest.controllers;
 
-import com.deusley.api_rest.domain.User;
+import com.deusley.api_rest.dto.UserDTO;
 import com.deusley.api_rest.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
 
-        return ResponseEntity.ok().body(service.findById(id));
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 
 
--- src/main/java/com/deusley/api_rest/dto/UserDTO.java
+++ src/main/java/com/deusley/api_rest/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.deusley.api_rest.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,8 @@ public class UserDTO {
     private Integer id;
     private String name;
     private String email;
+
+    @JsonIgnore
     private String password;
 
 }
