59dc1fd36cca98cf64752085a8050ad59dab43b7
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2023-03-10 03:42:29 -0300

    impls/PostMtd/

--- src/main/java/com/deusley/api_rest/services/UserService.java
+++ src/main/java/com/deusley/api_rest/services/UserService.java
@@ -1,13 +1,14 @@
 package com.deusley.api_rest.services;
 
 import com.deusley.api_rest.domain.User;
+import com.deusley.api_rest.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
 
     User findById(Integer id);
-
     List<User> findAll();
+    User create(UserDTO obj);
 
 }
--- src/main/java/com/deusley/api_rest/services/impl/UserServiceImpl.java
+++ src/main/java/com/deusley/api_rest/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.deusley.api_rest.services.impl;
 
 import com.deusley.api_rest.domain.User;
+import com.deusley.api_rest.dto.UserDTO;
 import com.deusley.api_rest.exceptions.ObjectNotFoundException;
 import com.deusley.api_rest.repositories.UserRepository;
 import com.deusley.api_rest.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository rep;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findById(Integer id) {
         Optional<User> obj = rep.findById(id);
@@ -24,5 +29,10 @@ public class UserServiceImpl implements UserService {
 
     public List<User> findAll(){
         return rep.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return rep.save(mapper.map(obj, User.class));
     }
 }
